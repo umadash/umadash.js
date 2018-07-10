e5050f3c9009ce73b7e07d8aefa2edd5b5dde6b8
umadash <yamato@umadash.com>
2018-07-10 12:11:53 +0900

    delete and move

--- test/js/test.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="events/EventDispatcher.ts" />
-/// <reference path="events/EventDispatcher.ts" />
-
-namespace umadash {
-    export function getVersion(): string {
-        return '0.0.1';
-    }
-}
