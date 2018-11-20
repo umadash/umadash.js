2e180c663379b35168361cc224c185132d8a4628
Yamato Hashimoto <hashimoto-yamato@blue-puddle.com>
2018-11-20 11:05:41 +0900

    update jqueryutil

--- util/JqueryUtil.ts
+++ util/JqueryUtil.ts
@@ -4,16 +4,9 @@ import {Easing} from '../tween/Easing'
 
 export class JqueryUtil {
 
-  public static removeClass($target: JQuery, pattern: string): JQuery {
-    $target.removeClass(function(index, className) {
-        const str = '\\b' + pattern + '\\S+';
-        const reg = new RegExp(str, 'g');
-        return (className.match(reg) || []).join(' ');
-    });
-    return $target;
-  }
+  
 
-  public static fadeTo($target, opacity, duration, easing = Easing.linear, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
+  public static fadeTo($target: JQuery, opacity: number, duration: number, easing = Easing.linear, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
     const o = { value: parseFloat($target.css('opacity')) };
     const tween = new DoTween(o, {value: opacity}, null, duration, easing, () => {
       if (opacity > 0) {
@@ -34,11 +27,11 @@ export class JqueryUtil {
   }
 
 
-  public static fadeOut(target, duration, easing, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
+  public static fadeOut(target: JQuery, duration: number, easing: any, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
     return JqueryUtil.fadeTo(target, 0, duration, easing, switchDisplayTo, switchVisibility, execute);
   }
 
-  public static fadeIn(target, duration, easing, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
+  public static fadeIn(target: JQuery, duration: number, easing: any, switchDisplayTo = "", switchVisibility = false, execute = false): Command {
     return JqueryUtil.fadeTo(target, 1, duration, easing, switchDisplayTo, switchVisibility, execute);
   }
 
@@ -54,4 +47,26 @@ export class JqueryUtil {
     return (top >= scrollTop && top < scrollTop + windowHeight);
   }
 
+  /**
+   * jQueryオブジェクトに付与されている、
+   * 特定のパターンのclassを取り除く
+   * @param $target 
+   * @param pattern 
+   */
+  public static removeClass($target: JQuery, pattern: string): JQuery {
+    $target.removeClass(function(index, className) {
+        const str = '\\b' + pattern + '\\S+';
+        const reg = new RegExp(str, 'g');
+        return (className.match(reg) || []).join(' ');
+    });
+    return $target;
+  }
+
+  /**
+   * 自分自身の要素を含んだHTMLを取得
+   * @param target 
+   */
+  public static getSelfHTML(target: JQuery): string {
+    return target.prop('outerHTML');
+  }
 }
